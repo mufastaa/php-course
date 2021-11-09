9fbb250ae516559c272747dfe50e242e603dbfb2
mufastaa <ishimwep21@gmail.com>
2021-11-09 05:28:16 -0800

    functions in php

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function hello()
+{
+    echo 'Hello I am mufastaa<br>';
+}
+
+hello();
+
+
+
+function sum($a, $b)
+{
+    return $a + $b;
+}
+
+echo sum(4,5).'<br>';
+echo sum(9,10);
+
+
+
